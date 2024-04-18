ec7ef8796f58e8ed91c98421ef1783d74bcbab7f
GiuDiMax <giuseppedimauro95@gmail.com>
2024-04-18 09:07:01 +0200

    edit

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+const baseUrl = "/comunichE-Giamo"
+
 function leafmap(){
 	map = L.map('map',
 	{
@@ -12,10 +14,10 @@ function leafmap(){
 	}).addTo(map);
 	
 	var marker = L.marker([40.583382253095984, 16.7503409628624]).addTo(map);
-	marker.bindPopup("<b>Parrocchia Gesù Risorto</b><br>Prova test 123, <a href='/mariasantissima'>Scopri di più</a>");
+	marker.bindPopup("<b>Parrocchia Gesù Risorto</b><br>Prova test 123, <a href='"+baseUrl+"/mariasantissima'>Scopri di più</a>");
 	
 	marker = L.marker([40.57984587469258, 16.761520409592844]).addTo(map);
-	marker.bindPopup("<b>Gravina di Rivolta</b><br>Prova test 123, <a href='/mariasantissima'>Scopri di più</a>");
+	marker.bindPopup("<b>Gravina di Rivolta</b><br>Prova test 123, <a href='"+baseUrl+"/mariasantissima'>Scopri di più</a>");
 }
 
 
