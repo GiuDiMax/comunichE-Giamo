9c0d934431924c1270ad3edcbe2a1734f63db960
GiuDiMax <giuseppedimauro95@gmail.com>
2024-04-22 11:14:26 +0200

    edit

--- map.js
+++ map.js
@@ -15,19 +15,19 @@
 	marker.bindPopup("<b>Kaleidoscopio Lab</b><br>Studio fotografico, <a href='"+baseUrl+"/kaleidoscopio.html'>Scopri di più</a>");
 
 	marker = L.marker([40.5794886,16.7571049]).addTo(map);
-	marker.bindPopup("<b>MuPa</b><br>Prova test 123, <a href='"+baseUrl+"/mupa.html'>Scopri di più</a>");
+	marker.bindPopup("<b>MuPa</b><br>Museo-Palazzo Multimediale, <a href='"+baseUrl+"/mupa.html'>Scopri di più</a>");
 
 	marker = L.marker([40.5905576,16.6888547]).addTo(map);
-	marker.bindPopup("<b>cava di ginosa</b><a href='"+baseUrl+"/cavadiginosa.html.html'>Scopri di più</a>");
+	marker.bindPopup("<b>Cava di ginosa</b><a href='"+baseUrl+"/cavadiginosa.html.html'>Scopri di più</a>");
 
 	marker = L.marker([40.57883525174568,16.7569409]).addTo(map);
 	marker.bindPopup("<b>Sartoria inglese</b><br><a href='"+baseUrl+"/sartoriainglese.html'>Scopri di più</a>");
 
 	marker = L.marker([40.5617461,16.7660327]).addTo(map);
-	marker.bindPopup("<b>Museo degli attrezzi</b><br>Un museo privato locale <a href='"+baseUrl+"/museolocale.html'>Scopri di più</a>");
+	marker.bindPopup("<b>Museo degli attrezzi</b><br>Il museo del signor Franco <a href='"+baseUrl+"/museolocale.html'>Scopri di più</a>");
 
 	marker = L.marker([40.584236109645275, 16.759024364097627]).addTo(map);
-	marker.bindPopup("<b>Ulivo pensante</b><br>Ulivo con una faccia munita di occhi, naso e sopraciglio <a href='"+baseUrl+"/ulivopensante.html'>Scopri di più</a>");
+	marker.bindPopup("<b>Ulivo pensante</b><br>Un ulivo con una faccia munita di occhi, naso e sopraciglio <a href='"+baseUrl+"/ulivopensante.html'>Scopri di più</a>");
 }
 
 
